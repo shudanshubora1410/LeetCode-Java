36be1bfb47e3d1149045dd944dc5971bca68ba78
shudanshubora1410 <shudanshubora1410@gmail.com>
2025-12-31 19:52:02 +0530

    Add Valid Sudoku solution

---
+++ Medium/ValidSudoku.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> seen = new HashSet<>();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c != '.') {
+                    if (!seen.add(c + " in row " + i) ||
+                        !seen.add(c + " in col " + j) ||
+                        !seen.add(c + " in box " + (i / 3) + "-" + (j / 3))) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}

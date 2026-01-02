79e4f15730ccab26756a9f782c5e494bbf5db2e4
shudanshubora1410 <shudanshubora1410@gmail.com>
2026-01-02 17:50:07 +0530

    Add N-Repeated Element solution

---
+++ Easy/NRepeatedElement.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return num;
+            }
+            set.add(num);
+        }
+        return -1;
+    }
+}

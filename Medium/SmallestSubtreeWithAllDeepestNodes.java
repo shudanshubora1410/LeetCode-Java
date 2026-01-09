800f547d7b5287fdb886d276b91a3017770943ce
shudanshubora1410 <shudanshubora1410@gmail.com>
2026-01-09 10:59:31 +0530

    Add Smallest Subtree with all the Deepest Nodes solution

--- Medium/SmallestSubtreeWithAllDeepestNodes.java
+++ Medium/SmallestSubtreeWithAllDeepestNodes.java
@@ -1,6 +1,21 @@
-import java.util.Arrays;
 class Solution {
-    public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        return dfs(root).node;
+    }
+    Result dfs(TreeNode n){
+        if(n == null) return new Result(null, 0);
+        Result l = dfs(n.left);
+        Result r = dfs(n.right);
+        if(l.depth > r.depth) return new Result(l.node, l.depth + 1);
+        if(r.depth > l.depth) return new Result(r.node, r.depth + 1);
+        return new Result(n, l.depth + 1);
+    }
+    static class Result {
+        TreeNode node;
+        int depth;
+        Result(TreeNode n, int d){
+            node = n;
+            depth = d;
+        }
     }
 }

960098f622550af9b9b47f7fd68861133e747db0
shudanshubora1410 <shudanshubora1410@gmail.com>
2026-01-11 10:33:00 +0530

    Add Binary Tree Inorder Traversal solution

---
+++ Easy/BinaryTreeInorderTraversal.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode cur = root;
+        while(cur != null || !st.isEmpty()){
+            while(cur != null){
+                st.push(cur);
+                cur = cur.left;
+            }
+            cur = st.pop();
+            res.add(cur.val);
+            cur = cur.right;
+        }
+        return res;
+    }
+}

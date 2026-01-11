62c60b8ff5e9c0deb4449a9ff1b0a53d831c3590
shudanshubora1410 <shudanshubora1410@gmail.com>
2026-01-11 10:27:08 +0530

    Add Reverse Linked List II solution

---
+++ Medium/ReverseLinkedListII.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(head == null || left == right) return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        for(int i = 1; i < left; i++){
+            prev = prev.next;
+        }
+        ListNode cur = prev.next;
+        for(int i = 0; i < right - left; i++){
+            ListNode next = cur.next;
+            cur.next = next.next;
+            next.next = prev.next;
+            prev.next = next;
+        }
+        return dummy.next;
+    }
+}

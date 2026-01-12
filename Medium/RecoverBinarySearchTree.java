c9c7535cd9e7aad0ed351a6ba74b1c93d29c7b60
shudanshubora1410 <shudanshubora1410@gmail.com>
2026-01-12 17:01:54 +0530

    Add Recover Binary Search Tree solution

---
+++ Medium/RecoverBinarySearchTree.java
@@ -0,0 +1,23 @@
+class Solution {
+    TreeNode first = null;
+    TreeNode second = null;
+    TreeNode prev = null;
+
+    public void recoverTree(TreeNode root) {
+        inorder(root);
+        int t = first.val;
+        first.val = second.val;
+        second.val = t;
+    }
+
+    void inorder(TreeNode node){
+        if(node == null) return;
+        inorder(node.left);
+        if(prev != null && prev.val > node.val){
+            if(first == null) first = prev;
+            second = node;
+        }
+        prev = node;
+        inorder(node.right);
+    }
+}

c031f35f5a574f98487b5c9faa94b727e7142685
shudanshubora1410 <shudanshubora1410@gmail.com>
2026-01-13 15:56:10 +0530

    Add Binary Tree Level Order Traversal solution

---
+++ Medium/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(root == null) return res;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+            for(int i = 0; i < size; i++){
+                TreeNode cur = q.poll();
+                level.add(cur.val);
+                if(cur.left != null) q.add(cur.left);
+                if(cur.right != null) q.add(cur.right);
+            }
+            res.add(level);
+        }
+        return res;
+    }
+}

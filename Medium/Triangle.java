8e91f41a0822a923fbd974c228bef640cf9aa24e
shudanshubora1410 <shudanshubora1410@gmail.com>
2026-01-17 10:12:09 +0530

    Add Triangle minimum path sum solution

---
+++ Medium/Triangle.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        for(int i = n - 2; i >= 0; i--) {
+            for(int j = 0; j < triangle.get(i).size(); j++) {
+                int down = triangle.get(i + 1).get(j);
+                int diag = triangle.get(i + 1).get(j + 1);
+                triangle.get(i).set(j, triangle.get(i).get(j) + Math.min(down, diag));
+            }
+        }
+        return triangle.get(0).get(0);
+    }
+}

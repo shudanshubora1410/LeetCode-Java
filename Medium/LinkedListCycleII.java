a2e48a11ec754a9910e1882a8ec6bf3293c394dd
shudanshubora1410 <shudanshubora1410@gmail.com>
2026-01-20 10:15:25 +0530

    Add Linked List Cycle II solution

---
+++ Medium/LinkedListCycleII.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) return null;
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                ListNode ptr = head;
+                while (ptr != slow) {
+                    ptr = ptr.next;
+                    slow = slow.next;
+                }
+                return ptr;
+            }
+        }
+        return null;
+    }
+}

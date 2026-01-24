26bd00d5d1a5adf78bcd98263cffb40a56e9b2b2
shudanshubora1410 <shudanshubora1410@gmail.com>
2026-01-24 15:18:46 +0530

    Add Sort List merge sort solution

---
+++ Medium/SortList.java
@@ -0,0 +1,38 @@
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode mid = slow.next;
+        slow.next = null;
+
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+
+        return merge(left, right);
+    }
+
+    private ListNode merge(ListNode a, ListNode b) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while (a != null && b != null) {
+            if (a.val <= b.val) {
+                curr.next = a;
+                a = a.next;
+            } else {
+                curr.next = b;
+                b = b.next;
+            }
+            curr = curr.next;
+        }
+
+        curr.next = (a != null) ? a : b;
+        return dummy.next;
+    }
+}

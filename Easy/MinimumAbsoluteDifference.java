5375f6af2b30b636cf8f38d4f3222b530f45be3f
shudanshubora1410 <shudanshubora1410@gmail.com>
2026-01-26 09:34:54 +0530

    Add Minimum Absolute Difference solution

---
+++ Easy/MinimumAbsoluteDifference.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> res = new ArrayList<>();
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            min = Math.min(min, arr[i + 1] - arr[i]);
+        }
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i + 1] - arr[i] == min) {
+                res.add(Arrays.asList(arr[i], arr[i + 1]));
+            }
+        }
+        return res;
+    }
+}

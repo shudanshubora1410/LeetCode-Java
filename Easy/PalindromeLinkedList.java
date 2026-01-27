29ede87ff29bc9f83995c4ea5a650f41bf3372d3
shudanshubora1410 <shudanshubora1410@gmail.com>
2026-01-27 15:49:23 +0530

    Add Palindrome Linked List solution

---
+++ Easy/PalindromeLinkedList.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) return true;
+
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode prev = null, curr = slow;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        ListNode left = head, right = prev;
+        while (right != null) {
+            if (left.val != right.val) return false;
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+    }
+}

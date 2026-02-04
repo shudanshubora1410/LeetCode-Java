573cfa7a52eb1d52b46fb84bd2ce1eb5a286b8e6
shudanshubora1410 <shudanshubora1410@gmail.com>
2026-02-04 15:34:41 +0530

    Add Maximum Odd Binary Number solution

---
+++ Easy/MaximumOddBinaryNumber.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String maximumOddBinaryNumber(String s) {
+        int ones = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '1') ones++;
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < ones - 1; i++) {
+            result.append('1');
+        }
+
+        for (int i = ones; i < s.length(); i++) {
+            result.append('0');
+        }
+
+        result.append('1');
+        return result.toString();
+    }
+}

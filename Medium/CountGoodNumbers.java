90741356a0e940615f4781d6f55b193835cecebf
shudanshubora1410 <shudanshubora1410@gmail.com>
2026-02-12 16:48:08 +0530

    Add solution for Count Good Numbers

---
+++ Medium/CountGoodNumbers.java
@@ -0,0 +1,26 @@
+class Solution {
+    private long MOD = 1_000_000_007;
+
+    public int countGoodNumbers(long n) {
+        long evenPositions = (n + 1) / 2;
+        long oddPositions = n / 2;
+
+        long firstPart = power(5, evenPositions);
+        long secondPart = power(4, oddPositions);
+
+        return (int) ((firstPart * secondPart) % MOD);
+    }
+
+    private long power(long base, long exp) {
+        long res = 1;
+        base %= MOD;
+        while (exp > 0) {
+            if (exp % 2 == 1) {
+                res = (res * base) % MOD;
+            }
+            base = (base * base) % MOD;
+            exp /= 2;
+        }
+        return res;
+    }
+}

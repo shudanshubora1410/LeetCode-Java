239b58801c1ec6b453e41a492b571dac2f9aa45b
shudanshubora1410 <shudanshubora1410@gmail.com>
2026-02-16 19:03:16 +0530

    Add Implement Queue using Stacks solution

---
+++ Easy/ImplementQueueUsingStacks.java
@@ -0,0 +1,32 @@
+class MyQueue {
+
+    private java.util.Stack<Integer> in;
+    private java.util.Stack<Integer> out;
+
+    public MyQueue() {
+        in = new java.util.Stack<>();
+        out = new java.util.Stack<>();
+    }
+
+    public void push(int x) {
+        in.push(x);
+    }
+
+    public int pop() {
+        peek();
+        return out.pop();
+    }
+
+    public int peek() {
+        if (out.isEmpty()) {
+            while (!in.isEmpty()) {
+                out.push(in.pop());
+            }
+        }
+        return out.peek();
+    }
+
+    public boolean empty() {
+        return in.isEmpty() && out.isEmpty();
+    }
+}

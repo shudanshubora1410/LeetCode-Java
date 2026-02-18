d4a66551e89a3a060c376dbda7cf8599751bb4e7
shudanshubora1410 <shudanshubora1410@gmail.com>
2026-02-18 16:05:35 +0530

    Add solution for Asteroid Collision

---
+++ Medium/AsteroidCollision.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        java.util.Stack<Integer> stack = new java.util.Stack<>();
+        
+        for (int asteroid : asteroids) {
+            boolean destroyed = false;
+            
+            while (!stack.isEmpty() && asteroid < 0 && stack.peek() > 0) {
+                int top = stack.peek();
+                if (top < -asteroid) {
+                    stack.pop();
+                } else if (top == -asteroid) {
+                    stack.pop();
+                    destroyed = true;
+                    break;
+                } else {
+                    destroyed = true;
+                    break;
+                }
+            }
+            
+            if (!destroyed) {
+                stack.push(asteroid);
+            }
+        }
+        
+        int[] result = new int[stack.size()];
+        for (int i = result.length - 1; i >= 0; i--) {
+            result[i] = stack.pop();
+        }
+        
+        return result;
+    }
+}

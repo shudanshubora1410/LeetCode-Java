27b20d39e18a257cf0a249ca3d1009c38e2ef84f
shudanshubora1410 <shudanshubora1410@gmail.com>
2026-02-20 15:50:59 +0530

    Add solution for Fruits Into Baskets II

---
+++ Easy/FruitsIntoBasketsII.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int n = fruits.length;
+        int unplaced = 0;
+        boolean[] used = new boolean[n];
+        
+        for (int i = 0; i < n; i++) {
+            boolean placed = false;
+            for (int j = 0; j < n; j++) {
+                if (!used[j] && baskets[j] >= fruits[i]) {
+                    used[j] = true;
+                    placed = true;
+                    break;
+                }
+            }
+            if (!placed) {
+                unplaced++;
+            }
+        }
+        
+        return unplaced;
+    }
+}

271da2bee657c7a9bf36f5e46eebaaaf97993939
shudanshubora1410 <shudanshubora1410@gmail.com>
2026-02-21 21:15:39 +0530

    Add solution for Prime Number of Set Bits in Binary Representation

---
+++ Easy/PrimeNumberOfSetBitsInBinaryRepresentation.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int countPrimeSetBits(int left, int right) {
+        int count = 0;
+        
+        for (int i = left; i <= right; i++) {
+            int bits = Integer.bitCount(i);
+            if (isPrime(bits)) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+    
+    private boolean isPrime(int n) {
+        if (n < 2) return false;
+        if (n == 2) return true;
+        if (n % 2 == 0) return false;
+        for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}

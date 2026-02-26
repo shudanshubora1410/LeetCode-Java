aba7fb6141ef1a368989dc6646a9a523b4457556
shudanshubora1410 <shudanshubora1410@gmail.com>
2026-02-26 18:31:04 +0530

    Add solution for Number of Steps to Reduce a Number in Binary Representation to One

---
+++ Medium/NumberOfStepsToReduceNumberInBinaryRepresentationToOne.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numSteps(String s) {
+        int steps = 0;
+        int carry = 0;
+        
+        for (int i = s.length() - 1; i > 0; i--) {
+            int digit = s.charAt(i) - '0' + carry;
+            
+            if (digit % 2 == 1) {
+                steps += 2;
+                carry = 1;
+            } else {
+                steps += 1;
+            }
+        }
+        
+        return steps + carry;
+    }
+}

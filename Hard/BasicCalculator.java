f76d6cec65b086647817c4c48082897c39b2ea56
shudanshubora1410 <shudanshubora1410@gmail.com>
2026-03-05 07:29:42 +0530

    Add solution for Basic Calculator

---
+++ Hard/BasicCalculator.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int calculate(String s) {
+        java.util.Stack<Integer> stack = new java.util.Stack<>();
+        int result = 0;
+        int num = 0;
+        int sign = 1;
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if (Character.isDigit(c)) {
+                num = num * 10 + (c - '0');
+            } else if (c == '+') {
+                result += sign * num;
+                num = 0;
+                sign = 1;
+            } else if (c == '-') {
+                result += sign * num;
+                num = 0;
+                sign = -1;
+            } else if (c == '(') {
+                stack.push(result);
+                stack.push(sign);
+                result = 0;
+                sign = 1;
+            } else if (c == ')') {
+                result += sign * num;
+                num = 0;
+                result *= stack.pop();
+                result += stack.pop();
+            }
+        }
+        
+        result += sign * num;
+        
+        return result;
+    }
+}

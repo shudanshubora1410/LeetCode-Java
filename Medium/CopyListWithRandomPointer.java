654603a800ee64c21cc219077c6ed43b2d0183ca
Shudanshu Bora <shudanshubora1410@gmail.com>
2026-03-11 17:24:23 +0530

    Add solution for Copy List with Random Pointer

---
+++ Medium/CopyListWithRandomPointer.java
@@ -0,0 +1,24 @@
+class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+        
+        java.util.Map<Node, Node> map = new java.util.HashMap<>();
+        
+        Node curr = head;
+        while (curr != null) {
+            map.put(curr, new Node(curr.val));
+            curr = curr.next;
+        }
+        
+        curr = head;
+        while (curr != null) {
+            map.get(curr).next = map.get(curr.next);
+            map.get(curr).random = map.get(curr.random);
+            curr = curr.next;
+        }
+        
+        return map.get(head);
+    }
+}

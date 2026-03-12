1176fa9c57aef72a5ab242a1c6144e3cba3d9f72
Shudanshu Bora <shudanshubora1410@gmail.com>
2026-03-12 20:29:19 +0530

    Add solution for Summary Ranges

---
+++ Easy/SummaryRanges.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>();
+        
+        if (nums.length == 0) {
+            return result;
+        }
+        
+        int start = nums[0];
+        int end = nums[0];
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == end + 1) {
+                end = nums[i];
+            } else {
+                if (start == end) {
+                    result.add(String.valueOf(start));
+                } else {
+                    result.add(start + "->" + end);
+                }
+                start = nums[i];
+                end = nums[i];
+            }
+        }
+        
+        if (start == end) {
+            result.add(String.valueOf(start));
+        } else {
+            result.add(start + "->" + end);
+        }
+        
+        return result;
+    }
+}

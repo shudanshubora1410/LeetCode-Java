3fdf2aae380e5c9afd174686be5b078c5908a60f
Shudanshu Bora <shudanshubora1410@gmail.com>
2026-03-16 18:53:29 +0530

    Add solution for Course Schedule II

---
+++ Medium/CourseScheduleII.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[] indegree = new int[numCourses];
+        java.util.List<java.util.List<Integer>> graph = new java.util.ArrayList<>();
+        
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new java.util.ArrayList<>());
+        }
+        
+        for (int[] pre : prerequisites) {
+            graph.get(pre[1]).add(pre[0]);
+            indegree[pre[0]]++;
+        }
+        
+        java.util.Queue<Integer> queue = new java.util.LinkedList<>();
+        
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        
+        int[] result = new int[numCourses];
+        int idx = 0;
+        
+        while (!queue.isEmpty()) {
+            int course = queue.poll();
+            result[idx++] = course;
+            
+            for (int next : graph.get(course)) {
+                indegree[next]--;
+                if (indegree[next] == 0) {
+                    queue.offer(next);
+                }
+            }
+        }
+        
+        if (idx != numCourses) {
+            return new int[0];
+        }
+        
+        return result;
+    }
+}
